dc4b9e55dac848ef24082a73b320154985b35094
Guido Hornig <hornig@actxc.de>
2018-06-10 16:06:00 +0200

    correction of translation

--- blocks/mycourses/lang/de/block_mycourses.php
+++ blocks/mycourses/lang/de/block_mycourses.php
@@ -47,6 +47,6 @@ $string['showsummarydesc'] = 'Zusammenfassung in der Liste anzeigen';
 $string['title'] = 'Meine Kurse';
 $string['back'] = 'Zurück';
 $string['noavailable'] = 'Keine Kurse, die gestartet werden möchten';
-$string['noinprogress'] = 'Keine Kurse in arbeit';
+$string['noinprogress'] = 'Keine angefangenen Kurse';
 $string['nocompleted'] = 'Keine abgeschlossenen Kurse';
 
